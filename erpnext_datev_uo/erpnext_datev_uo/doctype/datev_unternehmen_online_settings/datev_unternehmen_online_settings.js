609433f912cf126e64ee760243a298ebef2b09b2
barredterra <14891507+barredterra@users.noreply.github.com>
2021-12-06 14:19:01 +0100

    style: format js

--- erpnext_datev_uo/erpnext_datev_uo/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.js
+++ erpnext_datev_uo/erpnext_datev_uo/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.js
@@ -1,15 +1,15 @@
 // Copyright (c) 2021, Alyf and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DATEV Unternehmen Online Settings', {
-	refresh: function(frm) {
-		frm.set_query("print_format", "datev_voucher_config", function(doc, cdt, cdn) {
+frappe.ui.form.on("DATEV Unternehmen Online Settings", {
+	refresh: function (frm) {
+		frm.set_query("print_format", "datev_voucher_config", function (doc, cdt, cdn) {
 			let row = locals[cdt][cdn];
-			return{
+			return {
 				filters: {
 					"doc_type": row.voucher_type
 				}
-			}
+			};
 		});
 	}
 });
