4d36982dbed0482bb9c8d3ffb03d4023f626d86c
barredterra <14891507+barredterra@users.noreply.github.com>
2021-12-06 14:07:58 +0100

    Revert "feat: add email recipients dynamically"
    This reverts commit 550915fd8a798895b60aa9aa2671d97ec7e9dc12.

--- erpnext_datev_uo/erpnext_datev_uo/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.js
+++ erpnext_datev_uo/erpnext_datev_uo/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.js
@@ -1,43 +1,15 @@
 // Copyright (c) 2021, Alyf and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("DATEV Unternehmen Online Settings", {
-	refresh: function (frm) {
-		frm.set_query("print_format", "datev_voucher_config", function (doc, cdt, cdn) {
+frappe.ui.form.on('DATEV Unternehmen Online Settings', {
+	refresh: function(frm) {
+		frm.set_query("print_format", "datev_voucher_config", function(doc, cdt, cdn) {
 			let row = locals[cdt][cdn];
-			return {
+			return{
 				filters: {
-					doc_type: row.voucher_type,
-				},
-			};
-		});
-
-		frappe.call({
-			method: "erpnext_datev_uo.erpnext_datev_uo.doctype.datev_unternehmen_online_settings.datev_unternehmen_online_settings.get_options_for_recipient",
-			callback: function (r) {
-				if (r && r.message) {
-					frm.email_options = r.message;
+					"doc_type": row.voucher_type
 				}
-			},
+			}
 		});
-	},
-});
-
-frappe.ui.form.on("DATEV Voucher Config", {
-	voucher_type: function (frm, cdt, cdn) {
-		let row = locals[cdt][cdn];
-		set_recipient_options(frm, row)
-	},
-});
-
-function set_recipient_options(frm, row) {
-	if (!row.voucher_type) {
-		return;
 	}
-
-	frm.fields_dict.datev_voucher_config.grid.update_docfield_property(
-		"recipient",
-		"options",
-		[""].concat(frm.email_options[row.voucher_type])
-	);
-}
+});
