68b423b9a362b470cdf42fa8a38dee9681106e72
barredterra <14891507+barredterra@users.noreply.github.com>
2021-12-09 17:48:10 +0100

    refactor: everything
    - move sender out of child table
    - fix bugs
    - use make_communication istead of sendmail so that emails shows up in the comments

--- erpnext_datev_uo/erpnext_datev_uo/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.py
+++ erpnext_datev_uo/erpnext_datev_uo/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.py
@@ -1,9 +1,10 @@
-# Copyright (c) 2021, Alyf and contributors
+# Copyright (c) 2021, ALYF GmbH and contributors
 # For license information, please see license.txt
 
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.core.doctype.communication.email import make as make_communication
 
 
 class DATEVUnternehmenOnlineSettings(Document):
@@ -12,59 +13,74 @@ class DATEVUnternehmenOnlineSettings(Document):
 
 def send(doc, method):
 	settings = frappe.get_single("DATEV Unternehmen Online Settings")
-
-	if not settings.enabled or not doc.items:
+	if not settings.enabled:
 		return
 
-	print_settings = settings.get(
-		"datev_voucher_config", filters={"voucher_type": doc.doctype}
-	)
-	print_settings = print_settings and print_settings[0]
-
-	if not print_settings or not doc.get(print_settings.recipient):
+	voucher_config = get_voucher_config(settings, doc.doctype)
+	if not voucher_config:
 		return
 
-	attachments = get_attachments(print_settings, doc)
-
-	frappe.sendmail(
-		recipients=[doc.get(print_settings.recipient)],
-		subject=f"{_(doc.doctype)}: {doc.name}",
-		sender=print_settings.sender,
-		message=_("New {0} {1} from ERPNext.").format(_(doc.doctype), doc.name),
-		reference_doctype=doc.doctype,
-		reference_name=doc.name,
-		attachments=attachments,
-	)
-
-
-def get_attachments(print_settings, doc):
-	lang = (
-		doc.language
-		or frappe.db.get_value(
-			"Print Format", print_settings.print_format, "default_print_language"
-		)
-		or frappe.db.get_single_value("System Settings", "language")
-	)
-	attachments = [
-		{
-			"print_format_attachment": 1,
-			"doctype": doc.doctype,
-			"name": doc.name,
-			"print_format": print_settings.print_format,
-			"lang": lang,
-		}
-	]
-
-	if doc.doctype == "Purchase Invoice":
-		attachments.extend(
-			frappe.get_all(
-				"File",
-				filters={
-					"attached_to_doctype": doc.doctype,
-					"attached_to_name": doc.name,
-				},
-				fields=["`tabFile`.name as fid"],
+	attachments = []
+	if doc.doctype == "Sales Invoice":
+		attachments.append(
+			get_print_config(
+				doc.doctype, doc.name, voucher_config.print_format, doc.language
 			)
 		)
+	elif doc.doctype == "Purchase Invoice":
+		attachments.extend(get_attached_files(doc.doctype, doc.name))
 
-	return attachments
+	make_communication(
+		doctype=doc.doctype,
+		name=doc.name,
+		content=_("New {0} {1} sent by the ERPNext-DATEV integration.").format(
+			_(doc.doctype), doc.name
+		),
+		subject=f"{_(doc.doctype)}: {doc.name}",
+		sender=frappe.get_value("Email Account", settings.sender, "email_id"),
+		recipients=[voucher_config.recipient],
+		communication_medium="Email",
+		send_email=True,
+		attachments=attachments,
+		communication_type="Automated Message",
+		ignore_permissions=True,
+	)
+
+
+def get_voucher_config(settings: DATEVUnternehmenOnlineSettings, doctype: str):
+	voucher_config = settings.get(
+		"datev_voucher_config", filters={"voucher_type": doctype}
+	)
+	if not voucher_config:
+		return
+
+	return voucher_config[0]
+
+
+def get_attached_files(doctype: str, docname: str):
+	return frappe.get_all(
+		"File",
+		filters={
+			"attached_to_doctype": doctype,
+			"attached_to_name": docname,
+		},
+		fields=["name as fid"],
+	)
+
+
+def get_print_config(
+	doctype: str, docname: str, print_format: str, language: str = None
+):
+	_language = (
+		language
+		or frappe.db.get_value("Print Format", print_format, "default_print_language")
+		or frappe.db.get_single_value("System Settings", "language")
+	)
+
+	return {
+		"print_format_attachment": 1,
+		"doctype": doctype,
+		"name": docname,
+		"print_format": print_format,
+		"lang": _language,
+	}
