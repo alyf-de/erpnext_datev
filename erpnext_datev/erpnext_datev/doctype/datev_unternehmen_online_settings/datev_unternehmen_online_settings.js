62350344ee0c34646308cd1849311cf1dd85ca19
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-08-05 12:58:29 +0200

    feat(DATEV): filter voucher emails by document fields (#36)

--- erpnext_datev/erpnext_datev/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.js
+++ erpnext_datev/erpnext_datev/doctype/datev_unternehmen_online_settings/datev_unternehmen_online_settings.js
@@ -20,4 +20,51 @@ frappe.ui.form.on("DATEV Unternehmen Online Settings", {
 			};
 		});
 	},
+	datev_voucher_config_on_form_rendered(frm) {
+		const row = frm.cur_grid.doc;
+		const parent = frm.cur_grid.wrapper.find("[data-fieldname='filter_area']");
+		parent.empty();
+
+		if (!row.voucher_type) {
+			return;
+		}
+
+		const filters = row.filters && row.filters !== "[]" ? JSON.parse(row.filters) : [];
+
+		frappe.model.with_doctype(row.voucher_type, () => {
+			const filter_group = new frappe.ui.FilterGroup({
+				parent: parent,
+				doctype: row.voucher_type,
+				on_change: () => {
+					frappe.model.set_value(
+						row.doctype,
+						row.name,
+						"filters",
+						JSON.stringify(filter_group.get_filters())
+					);
+				},
+			});
+
+			filter_group.add_filters_to_filter_group(filters);
+		});
+	},
+});
+
+frappe.ui.form.on("DATEV Voucher Config", {
+	voucher_type(frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		frappe.model.set_value(row.doctype, row.name, "filters", "[]");
+
+		if (row.print_format) {
+			frappe.db.get_value("Print Format", row.print_format, "doc_type").then((r) => {
+				if (r.message.doc_type !== row.voucher_type) {
+					frappe.model.set_value(row.doctype, row.name, "print_format", "");
+				}
+			});
+		}
+
+		if (frm.cur_grid) {
+			frm.events.datev_voucher_config_on_form_rendered(frm);
+		}
+	},
 });
